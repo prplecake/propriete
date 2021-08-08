15142e1fe0056d18df94b94c0acf6dc8afefaa03
Matthew Jorgensen <matthew@jrgnsn.net>
2021-08-07 19:54:59 -0500

    Initial commit

---
+++ inventory/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+
+from .models import (
+	OwnerInfo,
+	PolicyInfo,
+	Tag,
+	Category,
+	Item,
+	Clothing,
+	Location,
+)
+
+
+class PolicyAdmin(admin.ModelAdmin):
+	list_display = ('policy_number', 'agent_name', 'company_name',)
+
+
+admin.site.register(OwnerInfo)
+admin.site.register(PolicyInfo, PolicyAdmin)
+admin.site.register(Tag)
+admin.site.register(Category)
+admin.site.register(Item)
+admin.site.register(Clothing)
+admin.site.register(Location)
