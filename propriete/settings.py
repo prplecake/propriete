eb29f42b5e4578b26fc5e1b73175d12a94ffbe52
Matthew Jorgensen <matthew@jrgnsn.net>
2021-08-08 17:11:14 -0500

    Set default date format

--- propriete/settings.py
+++ propriete/settings.py
@@ -119,10 +119,13 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
 
+# Default date format
+
+DATE_FORMAT = 'Y-m-d'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
