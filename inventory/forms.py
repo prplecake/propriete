b5d00569addfb8349d757e99bd08ce1a5bc24fb6
Matthew Jorgensen <matthew@jrgnsn.net>
2021-08-07 21:57:33 -0500

    Add ability to add/edit inventory items
    - Add ItemForm
    - Add generic forms.html template
    - Rename date_purchased to purchase_date to align with conventions
    - Update URLs

--- inventory/forms.py
+++ inventory/forms.py
@@ -3,6 +3,7 @@ from django.forms import ModelForm
 from .models import (
 	OwnerInfo,
 	PolicyInfo,
+	Item,
 )
 
 
@@ -27,4 +28,21 @@ class PolicyInfoForm(ModelForm):
 			'company_name',
 			'company_claims_phone',
 			'company_website',
+		)
+
+
+class ItemForm(ModelForm):
+	class Meta:
+		model = Item
+		fields = (
+			'description',
+			'location',
+			'make',
+			'serial_number',
+			'purchase_date',
+			'purchase_location',
+			'purchase_price',
+			'current_value',
+			'photo',
+			'notes',
 		)
