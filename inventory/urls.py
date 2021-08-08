8f1e07cb465554d81e5cb54a8d12bcc1ca74b338
Matthew Jorgensen <matthew@jrgnsn.net>
2021-08-07 21:42:33 -0500

    Add ability to create/edit OwnerInfo and PolicyInfo
    - Add links to /
    - Catch DoesNotExist errors
    - Add views
    - Add forms
    - Add templates
    - Update URLs

--- inventory/urls.py
+++ inventory/urls.py
@@ -6,4 +6,8 @@ app_name = 'inventory'
 urlpatterns = [
 	path('', views.IndexView.as_view(), name='index'),
 	path('inventory/', views.InventoryView.as_view(), name='inventory'),
+	path('inventory/owner_info_create/', views.owner_info_create),
+	path('inventory/policy_info_create/', views.policy_info_create),
+	path('inventory/owner_info_update/', views.owner_info_update),
+	path('inventory/policy_info_update/', views.policy_info_update),
 ]
