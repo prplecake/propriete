9b60e7758f9e46e772fcb8580c07f749d2c54a61
Matthew Jorgensen <matthew@jrgnsn.net>
2021-08-13 14:19:03 -0500

    Add names for {owner,policy}_info_{create,update} views

--- inventory/urls.py
+++ inventory/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
 	path('', views.IndexView.as_view(), name='index'),
 	path('inventory/', views.InventoryView.as_view(), name='inventory'),
 	
-	path('inventory/owner_info_create/', views.owner_info_create),
-	path('inventory/policy_info_create/', views.policy_info_create),
-	path('inventory/owner_info_update/', views.owner_info_update),
-	path('inventory/policy_info_update/', views.policy_info_update),
+	path('inventory/owner_info_create/', views.owner_info_create, name='owner_info_create'),
+	path('inventory/policy_info_create/', views.policy_info_create, name='policy_info_create'),
+	path('inventory/owner_info_update/', views.owner_info_update, name='owner_info_update'),
+	path('inventory/policy_info_update/', views.policy_info_update, name='policy_info_update'),
 	
 	path('inventory/item/add/', views.item_add, name='item_add'),
 	path('inventory/item/<int:id>/edit/', views.item_update, name='item_update'),
