57d17a97f6fed5408f2c702d4696ee26adbe28d9
Matthew Jorgensen <matthew@jrgnsn.net>
2021-11-01 00:59:02 -0500

    Fixes #9: Use class based views in inventory app

--- inventory/views.py
+++ inventory/views.py
@@ -1,192 +1,105 @@
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.utils.datastructures import MultiValueDictKeyError
-from django.views.generic import ListView
+from django.views.generic import ListView, View
 
 from .forms import (
-	ItemForm,
-	LocationForm,
-	ClothingForm,
+	ClothingAddForm,
+	ClothingUpdateForm,
+	ItemAddForm,
+	ItemUpdateForm,
+	LocationAddForm,
+	LocationUpdateForm,
 )
 
 from .models import (
+	Clothing,
 	Item,
 	Location,
-	Clothing,
 )
 
 from view_classes import (
 	BaseAddView,
-	BaseUpdateView,
 	BaseDeleteView,
+	BaseUpdateView,
 )
 
-@login_required
-def item_add(request):
-	add_another = False
-	if request.method == 'POST':
-		form = ItemForm(request.POST)
-		try:
-			if request.POST['add_another'] == 'on':
-				add_another = True
-		except MultiValueDictKeyError:
-			add_another = False
-		if form.is_valid():
-			form.save()
-			if add_another:
-				pass
-			else:
-				return HttpResponseRedirect('/inventory/')
-	form = ItemForm(
-		initial={
-			'add_another': add_another,
-		})
-	return render(request, 'inventory/item_form.html', {'form': form})
+
+class ItemAdd(BaseAddView):
+	form_class = ItemAddForm
+	template_name = 'inventory/item_form.html'
+	redirect_target = 'inventory:inventory'
 
 
-@login_required
-def item_update(request, id):
-	item = get_object_or_404(Item, id=id)
-	form = ItemForm(request.POST or None, instance=item)
-	if form.is_valid():
-		form.save()
-		return HttpResponseRedirect('/inventory/')
-
-	return render(
-		request,
-		'inventory/item_form.html',
-		{
-			'form': form,
-			'item': item,
-		}
-	)
+class ItemUpdate(BaseUpdateView):
+	model_class = Item
+	form_class = ItemUpdateForm
+	template_name = 'inventory/item_form.html'
+	redirect_target = 'inventory:inventory'
 
 
-@login_required
-def item_delete(request, id):
-	item = get_object_or_404(Item, id=id)
-	item.delete()
-	return HttpResponseRedirect('/inventory/')
+class ItemDelete(BaseDeleteView):
+	model_class = Item
+	redirect_target = 'inventory:inventory'
 
 
-@login_required
-def location_list(request):
-	locations = Location.objects.order_by('name')
-	return render(
-		request,
-		'inventory/location_list.html',
-		{
-			'locations': locations,
-		}
-	)
+class LocationList(LoginRequiredMixin, ListView):
+	model = Location
 
 
-@login_required
-def location_add(request):
-	add_another = False
-	if request.method == 'POST':
-		form = LocationForm(request.POST)
-		try:
-			if request.POST['add_another'] == 'on':
-				add_another = True
-		except MultiValueDictKeyError:
-			add_another = False
-		if form.is_valid():
-			form.save()
-			if add_another:
-				pass
-			else:
-				return HttpResponseRedirect('/inventory/locations/')
-	form = LocationForm(
-		initial={
-			'add_another': add_another,
-		})
-	return render(request, 'inventory/location_form.html', {'form': form})
+class LocationAdd(BaseAddView):
+	form_class = LocationAddForm
+	template_name = 'inventory/location_form.html'
+	redirect_target = 'inventory:location_list'
 
 
-@login_required
-def location_detail(request, id):
-	location = get_object_or_404(Location, id=id)
-	items = Item.objects.filter(location_id=location.id)
+class LocationDetail(LoginRequiredMixin, View):
+	def get(self, request, id, *args, **kwargs):
+		location = get_object_or_404(Location, id=id)
+		items = Item.objects.filter(location_id=location.id)
 
-	return render(
-		request,
-		'inventory/location_detail.html',
-		{
-			'location': location,
-			'item_list': items,
-		}
-	)
+		return render(
+			request,
+			'inventory/location_detail.html',
+			{
+				'location': location,
+				'item_list': items,
+			}
+		)
 
 
-@login_required
-def location_delete(request, id):
-	location = get_object_or_404(Location, id=id)
-	location.delete()
-	return HttpResponseRedirect('/inventory/locations/')
+class LocationUpdate(BaseUpdateView):
+	model_class = Location
+	form_class = LocationUpdateForm
+	template_name = 'inventory/location_form.html'
+	redirect_target = 'inventory:location_list'
+
+
+class LocationDelete(BaseDeleteView):
+	model_class = Location
+	redirect_target = 'inventory:location_list'
+
+
+class ClothingList(LoginRequiredMixin, ListView):
+	model = Clothing
+
+
+class ClothingAdd(BaseAddView):
+	form_class = ClothingAddForm
+	template_name = 'inventory/clothing_form.html'
+	redirect_target = 'inventory:clothing_list.hmtl'
+
+
+class ClothingUpdate(BaseUpdateView):
+	model_class = Clothing
+	form_class = ClothingUpdateForm
+	template_name = 'inventory/clothing_form.html'
+	redirect_target = 'inventory:clothing_list.hmtl'
+
+
+class ClothingDelete(BaseDeleteView):
+	model_class = Clothing
+	redirect_target = 'inventory:clothing_list.hmtl'
 
 
 class InventoryView(LoginRequiredMixin, ListView):
 	model = Item
-
-
-@login_required
-def clothing_list(request):
-	clothing = Clothing.objects.order_by('description')
-	return render(
-		request,
-		'inventory/clothing_list.html',
-		{
-			'clothing_list': clothing,
-		}
-	)
-
-
-@login_required
-def clothing_add(request):
-	add_another = False
-	if request.method == 'POST':
-		form = ClothingForm(request.POST)
-		try:
-			if request.POST['add_another'] == 'on':
-				add_another = True
-		except MultiValueDictKeyError:
-			add_another = False
-		if form.is_valid():
-			form.save()
-			if add_another:
-				pass
-			else:
-				return HttpResponseRedirect('/inventory/clothing/')
-	form = ClothingForm(
-		initial={
-			'add_another': add_another,
-		})
-	return render(request, 'inventory/clothing_form.html', {'form': form})
-
-
-@login_required
-def clothing_update(request, id):
-	clothing = get_object_or_404(Clothing, id=id)
-	form = ClothingForm(request.POST or None, instance=clothing)
-	if form.is_valid():
-		form.save()
-		return HttpResponseRedirect('/inventory/clothing/')
-
-	return render(
-		request,
-		'inventory/clothing_form.html',
-		{
-			'form': form,
-			'clothing': clothing,
-		}
-	)
-
-
-@login_required
-def clothing_delete(request, id):
-	clothing = get_object_or_404(Clothing, id=id)
-	clothing.delete()
-	return HttpResponseRedirect('/inventory/clothing/')
